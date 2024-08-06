7795553ca1f4a23bc325460ba65c21c9c0f0cea7
ArunShanmughan <arunshanmughan35@gmail.com>
2024-08-06 10:20:16 +0530

    leetcode done 3016

---
+++ 3016.js
@@ -0,0 +1,43 @@
+
+// 3016. Minimum Number of Pushes to Type Word II
+
+// You are given a string word containing lowercase English letters.
+
+// Telephone keypads have keys mapped with distinct collections of 
+// lowercase English letters, which can be used to form words by pushing 
+// them. For example, the key 2 is mapped with ["a","b","c"], we need to 
+// push the key one time to type "a", two times to type "b", and three times 
+// to type "c" .
+
+// It is allowed to remap the keys numbered 2 to 9 to distinct collections 
+// of letters. The keys can be remapped to any amount of letters, but each 
+// letter must be mapped to exactly one key. You need to find the minimum 
+// number of times the keys will be pushed to type the string word.
+
+// Return the minimum number of pushes needed to type word after remapping the keys.
+
+// An example mapping of letters to keys on a telephone keypad is given below. 
+// Note that 1, *, #, and 0 do not map to any letters.
+
+var word = "aabbccddeeffgghhiiiiii";
+
+var minimumPushes = function(word) {
+  let arr = []
+  let count = 0
+  word = word.split("").sort()
+  for(let i=0;i<word.length;i++){
+      let temp = 1
+      while(word[i]==word[i+1]){
+          temp++
+          i++
+      }
+      arr.push(temp)
+  }
+  arr.sort((a,b)=>b-a)
+  for(let i=0;i<arr.length;i++){
+      count+=(1+Math.floor(i/8))*arr[i]
+  }
+  return count;
+};
+
+console.log(minimumPushes(word))
