b4529eec0823af892615a89ec9b0bbe22e795b44
ArunShanmughan <arunshanmughan35@gmail.com>
2024-08-14 09:53:16 +0530

    added alternative code for 2351

--- 2351.js
+++ 2351.js
@@ -1,29 +1,41 @@
-
 // 2351. First Letter to Appear Twice
 
-// Given a string s consisting of lowercase English letters, 
+// Given a string s consisting of lowercase English letters,
 // return the first letter to appear twice.
 
 // Note:
 
-// A letter a appears twice before another letter b if the second occurrence of 
+// A letter a appears twice before another letter b if the second occurrence of
 // a is before the second occurrence of b.
 // s will contain at least one letter that appears twice.
 
 var s = "abccbaacz";
 
-var repeatedCharacter = function(s) {
+var repeatedCharacter = function (s) {
   let freq = {};
   for (let i = 0; i < s.length; i++) {
-      if (freq.hasOwnProperty(s[i])) {
-          freq[s[i]]++;
-          if (freq[s[i]] === 2) {
-              return s[i];
-          }
-      } else {
-          freq[s[i]] = 1;
-      }
-    }
+    if (freq.hasOwnProperty(s[i])) {
+      freq[s[i]]++;
+      if (freq[s[i]] === 2) {
+        return s[i];
+      }
+    } else {
+      freq[s[i]] = 1;
+    }
+  }
 };
 
-console.log(repeatedCharacter(s))
+console.log(repeatedCharacter(s));
+
+function repeatedChar(s) {
+  let seen = new Set();
+  for (let i = 0; i < s.length; i++) {
+    if (seen.has(s[i])) {
+      return s[i];
+    }
+    seen.add(s[i]);
+  }
+  return null;
+}
+
+console.log(repeatedChar(s))
